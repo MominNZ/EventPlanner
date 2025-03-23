c9b8c8a24f0f849b89180ec96c33e5256d88e296
Momin <mominnazar24@gmail.com>
2025-03-23 23:22:46 +0500

    Initial commit by MominNZ

---
+++ tests/events.test.js
@@ -0,0 +1,16 @@
+import request from 'supertest';
+import app from '../index.js';
+
+describe('Event Planner API', () => {
+    it('should register a new user', async () => {
+        const res = await request(app).post('/register').send({
+            username: 'testuser',
+            password: 'password123'
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe('User registered successfully');
+    });
+});
+
+
+//https://github.com/MominNZ/EventPlanner.git
